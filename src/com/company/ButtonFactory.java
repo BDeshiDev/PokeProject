22304143a0c190ac442630c7047c949f8a2d3989
USER <1705088@ugrad.cse.buet.ac.bd>
2019-02-07 17:53:13 +0600

    Working with css

--- src/com/company/ButtonFactory.java
+++ src/com/company/ButtonFactory.java
@@ -3,15 +3,25 @@ package com.company;
 import com.company.Pokemon.Moves.Move;
 import com.company.Pokemon.Pokemon;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 import java.beans.EventHandler;
 import java.io.IOException;
 
 public class ButtonFactory {
-
-    private static Button moveButton;
+    @FXML
+    private Button moveButton;
+    @FXML
+    private Label moveName;
+    @FXML
+    private Label moveType;
+    @FXML
+    private Label power;
 
     public static Button getSwapButton(int width, int height, Pokemon p){
         //load the fxml file here
@@ -22,17 +32,23 @@ public class ButtonFactory {
     }
 
     public static Button getMoveButton(int width, int height, Move m){
-        //load the fxml file here
+
         FXMLLoader loader=new FXMLLoader(ButtonFactory.class.getResource("Buttons.fxml"));
 
+        Button button=null;
         try {
-            moveButton = loader.load();
-            moveButton.setText(m.getName());
-        } catch (Exception e) {
-            System.out.println("button load failed");
-        }
+            button=loader.load();
+            AnchorPane anchorPane =(AnchorPane) button.getGraphic();
+            Label label1=(Label)anchorPane.getChildren().get(0);
+            label1.setText(m.getName());
+            Label label2=(Label)anchorPane.getChildren().get(1);
+            label2.setText("Type: "+m.type.name());
+            Label label3=(Label)anchorPane.getChildren().get(2);
+            label3.setText("Power: "+String.valueOf(m.power));
 
-        moveButton.setPrefSize(width,height);
-        return moveButton;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return button;
     }
 }
