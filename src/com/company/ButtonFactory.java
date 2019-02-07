54d8ac213fd884551697a16c02b1fdfdf050fa09
USER <1705088@ugrad.cse.buet.ac.bd>
2019-02-07 13:09:05 +0600

    Working with css

--- src/com/company/ButtonFactory.java
+++ src/com/company/ButtonFactory.java
@@ -3,11 +3,16 @@ package com.company;
 import com.company.Pokemon.Moves.Move;
 import com.company.Pokemon.Pokemon;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 
 import java.beans.EventHandler;
+import java.io.IOException;
 
 public class ButtonFactory {
+
+    private static Button moveButton;
+
     public static Button getSwapButton(int width, int height, Pokemon p){
         //load the fxml file here
         Button b = new Button(p.name);
@@ -18,8 +23,16 @@ public class ButtonFactory {
 
     public static Button getMoveButton(int width, int height, Move m){
         //load the fxml file here
-        Button b = new Button(m.getName());
-        b.setPrefSize(width,height);
-        return b;
+        FXMLLoader loader=new FXMLLoader(ButtonFactory.class.getResource("Buttons.fxml"));
+
+        try {
+            moveButton = loader.load();
+            moveButton.setText(m.getName());
+        } catch (Exception e) {
+            System.out.println("button load failed");
+        }
+
+        moveButton.setPrefSize(width,height);
+        return moveButton;
     }
 }
